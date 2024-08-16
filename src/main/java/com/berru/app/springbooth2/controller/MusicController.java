2e36e02606ccc9b0eacfd30faa5437f4a2836efc
berru <berruhanedar@icloud.com>
2024-08-16 03:04:30 +0300

    update

--- src/main/java/com/berru/app/springbooth2/controller/MusicController.java
+++ src/main/java/com/berru/app/springbooth2/controller/MusicController.java
@@ -3,57 +3,52 @@ package com.berru.app.springbooth2.controller;
 import com.berru.app.springbooth2.dto.MusicDTO;
 import com.berru.app.springbooth2.dto.NewMusicRequestDTO;
 import com.berru.app.springbooth2.dto.UpdateMusicRequestDTO;
-import com.berru.app.springbooth2.entity.Music;
+import com.berru.app.springbooth2.dto.MusicPaginationResponse;
 import com.berru.app.springbooth2.service.MusicService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
-@RestController //Buranın bir controller sınıfı olduğunu belirtmek için kullanıyoruz
+@RestController
 @RequestMapping("/musics")
 public class MusicController {
-    final MusicService musicService;
+    private final MusicService musicService;
 
     public MusicController(MusicService musicService) {
         this.musicService = musicService;
     }
 
-    // Müzik ekleme işlemi
     @PostMapping
     public ResponseEntity<MusicDTO> save(@RequestBody @Valid NewMusicRequestDTO newMusicRequestDTO) {
         return musicService.save(newMusicRequestDTO);
     }
 
     @GetMapping
-    public ResponseEntity<List<MusicDTO>> list() {
-        return musicService.list();
+    public ResponseEntity<MusicPaginationResponse> getMusics(
+            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize
+    ) {
+        return new ResponseEntity<>(musicService.list(pageNo, pageSize), HttpStatus.OK);
     }
 
-    // Belirli bir müziği ID'ye göre getir
     @GetMapping("/{id}")
     public ResponseEntity<MusicDTO> getById(@PathVariable int id) {
         return musicService.getById(id);
     }
-
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable int id) {
         return musicService.delete(id);
     }
 
-
     @PutMapping("/{id}")
     public ResponseEntity<MusicDTO> update(@PathVariable int id, @Valid @RequestBody UpdateMusicRequestDTO updateMusicRequestDTO) {
         return musicService.update(id, updateMusicRequestDTO);
     }
 
-
-
-
-    // Belirli bir genre'e ait müzikleri getir
     @GetMapping("/genre/{genreId}")
     public ResponseEntity<List<MusicDTO>> getByGenre(@PathVariable int genreId) {
         return musicService.getByGenre(genreId);
--- src/main/java/com/berru/app/springbooth2/service/MusicService.java
+++ src/main/java/com/berru/app/springbooth2/service/MusicService.java
@@ -1,27 +1,28 @@
 package com.berru.app.springbooth2.service;
 
 import com.berru.app.springbooth2.dto.MusicDTO;
+import com.berru.app.springbooth2.dto.MusicPaginationResponse;
 import com.berru.app.springbooth2.dto.NewMusicRequestDTO;
 import com.berru.app.springbooth2.dto.UpdateMusicRequestDTO;
 import com.berru.app.springbooth2.exception.NotFoundException;
-
 import com.berru.app.springbooth2.entity.Genre;
 import com.berru.app.springbooth2.entity.Music;
 import com.berru.app.springbooth2.repository.GenreRepository;
 import com.berru.app.springbooth2.repository.MusicRepository;
-import jakarta.validation.Valid;
-import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class MusicService {
-    final MusicRepository musicRepository;
-    final GenreRepository genreRepository;
+    private final MusicRepository musicRepository;
+    private final GenreRepository genreRepository;
 
     public MusicService(MusicRepository musicRepository, GenreRepository genreRepository) {
         this.musicRepository = musicRepository;
@@ -29,25 +30,19 @@ public class MusicService {
     }
 
     public ResponseEntity<MusicDTO> save(NewMusicRequestDTO newMusicRequestDTO) {
-        // Genre ID'sine sahip bir Genre nesnesi arar
         Genre genre = genreRepository.findById(newMusicRequestDTO.getGenreId())
                 .orElseThrow(() -> new NotFoundException("Genre not found"));
 
-        //Nesne Oluşturma Sürecini Basitleştirmek
-        // Lombok Builder ile DTO'dan bir Music nesnesi oluşturur
         Music music = Music.builder()
                 .name(newMusicRequestDTO.getName())
                 .genre(genre)
                 .build();
 
-        // Müzik nesnesini veritabanına kaydeder
         Music savedMusic = musicRepository.save(music);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(convertToDTO(savedMusic));
     }
 
-
-    //dış dünyaya veri gönderirken yapılan bir işlemdir.
     private MusicDTO convertToDTO(Music music) {
         MusicDTO musicDTO = new MusicDTO();
         musicDTO.setId(music.getId());
@@ -56,56 +51,51 @@ public class MusicService {
         return musicDTO;
     }
 
+    public MusicPaginationResponse list(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Page<Music> musicPage = musicRepository.findAll(pageable);
 
-    // Müzik nesnesini veritabanına kaydeder ve HTTP 201 (Created) yanıtı döner.
-
-
-    public ResponseEntity<List<MusicDTO>> list() {
-        List<Music> musics = musicRepository.findAll();
-        List<MusicDTO> musicDTOs = musics.stream()
+        List<MusicDTO> musicDTOList = musicPage.getContent().stream()
                 .map(this::convertToDTO)
                 .collect(Collectors.toList());
-        return ResponseEntity.ok(musicDTOs);
+
+        MusicPaginationResponse musicPaginationResponse = new MusicPaginationResponse();
+        musicPaginationResponse.setContent(musicDTOList);
+        musicPaginationResponse.setPageNo(musicPage.getNumber());
+        musicPaginationResponse.setPageSize(musicPage.getSize());
+        musicPaginationResponse.setTotalElements(musicPage.getTotalElements());
+        musicPaginationResponse.setTotalPages(musicPage.getTotalPages());
+        musicPaginationResponse.setLast(musicPage.isLast());
+
+        return musicPaginationResponse;
     }
 
-
-    // Belirli bir müziği ID'ye göre getir
     public ResponseEntity<MusicDTO> getById(int id) {
         Music music = musicRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Music not found"));
 
         return ResponseEntity.ok(convertToDTO(music));
-
     }
 
-    // Belirli bir müziği ID'ye göre güncelle
-    public ResponseEntity<MusicDTO> update(int id, @Valid UpdateMusicRequestDTO updateMusicRequestDTO) {
+    public ResponseEntity<MusicDTO> update(int id, UpdateMusicRequestDTO updateMusicRequestDTO) {
         try {
-            // Music ID kontrolü
             Music existingMusic = musicRepository.findById(id)
                     .orElseThrow(() -> new NotFoundException("Music not found"));
 
-            // Genre ID kontrolü
             Genre genre = genreRepository.findById(updateMusicRequestDTO.getGenreId())
                     .orElseThrow(() -> new NotFoundException("Genre not found"));
 
-            // Müzik adını güncelleme
             existingMusic.setName(updateMusicRequestDTO.getName());
             existingMusic.setGenre(genre);
 
-            // Müzik kaydını güncelleme
             Music updatedMusic = musicRepository.save(existingMusic);
 
             return ResponseEntity.ok(convertToDTO(updatedMusic));
         } catch (IllegalArgumentException ex) {
-            // İstemci hatası durumunda 400 Bad Request döndürülmesi
             return ResponseEntity.badRequest().body(null);
         }
     }
 
-
-
-    // Müzik silme işlemi
     public ResponseEntity<Void> delete(int id) {
         if (musicRepository.existsById(id)) {
             musicRepository.deleteById(id);
@@ -114,7 +104,6 @@ public class MusicService {
         throw new NotFoundException("Music not found");
     }
 
-    // Belirli bir genre'e ait müzikleri getirme işlemi
     public ResponseEntity<List<MusicDTO>> getByGenre(int genreId) {
         List<Music> musics = musicRepository.findByGenreId(genreId);
         List<MusicDTO> musicDTOs = musics.stream()
